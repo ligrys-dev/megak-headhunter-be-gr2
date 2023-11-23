4e9b38e90933bb765b487f94f0180f545b83f864
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-23 18:41:36 +0100

    change name to student-import

--- src/modules/csv/csv.controller.ts
+++
@@ -1,7 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { CsvService } from './csv.service';
-
-@Controller('csv')
-export class CsvController {
-  constructor(private readonly csvService: CsvService) {}
-}
--- src/modules/csv/csv.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CsvService } from './csv.service';
-import { CsvController } from './csv.controller';
-
-@Module({
-  controllers: [CsvController],
-  providers: [CsvService],
-})
-export class CsvModule {}
