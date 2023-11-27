7f86fecaf78f7d5f78b19de6bdd0dc4bbfeb5715
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-27 12:32:44 +0100

    add generate random password fn and implement it in createStudents

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -3,6 +3,7 @@ import { User } from './entities/user.entity';
 import { CreateStudentDto } from './dto/create-user.dto';
 import { Role } from 'src/types';
 import { hashPwd } from 'src/utils/handle-pwd';
+import { generateRandomPwd } from 'src/utils/generate-random-pwd';
 
 @Injectable()
 export class UserService {
@@ -11,24 +12,30 @@ export class UserService {
   }
 
   async createStudents(createStudentDtos: CreateStudentDto[]) {
+    const createdUsers: { newUser: User; password: string }[] = [];
+
     try {
       for (const createStudentDto of createStudentDtos) {
         const { email, ...data } = createStudentDto;
+        const password = generateRandomPwd();
+
         const user = new User();
         user.email = email;
         user.role = Role.STUDENT;
-        user.pwdHash = await hashPwd('haslo do wygenerowania');
+        user.pwdHash = await hashPwd(password);
+        const newUser = await user.save();
 
-        await user.save();
+        createdUsers.push({ newUser, password });
+
         // const student = new Student()
         // for (const [key, value] of Object.entries(data)) {
         //   student[key] = value;
         // }
         // student.email = email
         // await student.save()
-        console.log(data); //
+        console.log(data); // TODO to implement when student entitity will be implemented
       }
-      return { ok: true };
+      return createdUsers;
     } catch (e) {
       throw new Error(e);
     }
