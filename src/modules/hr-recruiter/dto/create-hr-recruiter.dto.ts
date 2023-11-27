b30f06476b4b395c2b7c602c913a8aa5df8e0bb4
DominikDO98 <123335551+DominikDO98@users.noreply.github.com>
2023-11-28 00:02:12 +0100

    dto finished

--- src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
+++ src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
@@ -1,1 +1,9 @@
-export class CreateHrRecruiterDto {}
+import { NewRecruiterEntity } from "src/types";
+
+export class CreateHrRecruiterDto implements NewRecruiterEntity{
+    id: number;
+    email: string;
+    fullName: string;
+    company: string;
+    maxReservedStudents: number;
+}
--- src/modules/hr-recruiter/dto/update-hr-recruiter.dto.ts
+++ src/modules/hr-recruiter/dto/update-hr-recruiter.dto.ts
@@ -1,4 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateHrRecruiterDto } from './create-hr-recruiter.dto';
 
-export class UpdateHrRecruiterDto extends PartialType(CreateHrRecruiterDto) {}
+export class UpdateHrRecruiterDto extends PartialType(CreateHrRecruiterDto) {
+    id: number;
+    email: string;
+    fullName: string;
+    company: string;
+    maxReservedStudents: number;
+}
