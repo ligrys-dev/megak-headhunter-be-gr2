71a157dcf4c04fa17e66b0dd7526efff69029283
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-27 12:09:20 +0100

    add redirect and handling by cache uploaed file

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Inject, Post } from '@nestjs/common';
 import { UserService } from './user.service';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Cache } from 'cache-manager';
+import { CreateStudentDto } from './dto/create-user.dto';
 
 @Controller('user')
 export class UserController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
+  ) {}
+
+  @Post('/students')
+  async createStudentUsers() {
+    const createStudentDtos: CreateStudentDto[] =
+      await this.cacheManager.get('students');
+    return this.userService.createStudents(createStudentDtos);
+  }
 }
