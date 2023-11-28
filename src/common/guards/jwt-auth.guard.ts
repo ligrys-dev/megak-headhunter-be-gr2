6a879c2bababa4bba4f67ac4526279654c12356d
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-28 10:12:19 +0100

    modified jwt auth guard to check is user is active

--- src/common/guards/jwt-auth.guard.ts
+++ src/common/guards/jwt-auth.guard.ts
@@ -1,5 +1,25 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, ExecutionContext } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Reflector } from '@nestjs/core';
+import { JwtService } from '@nestjs/jwt';
+import { UserEntity } from 'src/types';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly jwtService: JwtService,
+  ) {
+    super();
+  }
+
+  canActivate(context: ExecutionContext) {
+    const req = context.switchToHttp().getRequest();
+    const access_token = req.cookies.access_token;
+    const user: UserEntity = this.jwtService.decode(access_token);
+
+    if (!user.isActive) return false;
+
+    return super.canActivate(context);
+  }
+}
