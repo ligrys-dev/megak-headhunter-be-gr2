ad34f043a8f4676765d68a97c8f3208141947309
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-30 15:47:27 +0100

    applyed class validator on create recruiter dto

--- src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
+++ src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
@@ -1,9 +1,18 @@
+import { IsEmail, IsInt, IsNotEmpty, Max, Min } from 'class-validator';
 import { NewRecruiterEntity } from 'src/types';
 
 export class CreateHrRecruiterDto implements NewRecruiterEntity {
-  id: number;
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
   fullName: string;
+
+  @IsNotEmpty()
   company: string;
+
+  @IsInt()
+  @Min(1)
+  @Max(999)
   maxReservedStudents: number;
 }
