8b4361d9cf8a90309f43c3ad2bb8769386556561
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-30 11:47:50 +0100

    implement global Validoation Pipe

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import { getCorsConfig } from './config/cors.config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   app.enableCors(getCorsConfig);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3001);
 }
 bootstrap();
