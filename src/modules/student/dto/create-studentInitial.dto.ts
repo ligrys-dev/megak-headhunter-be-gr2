f5fa36b7797118b2b123b6b17f3bc1f2655a1d03
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-01 00:19:01 +0100

    add validation to dto

--- src/modules/student/dto/create-studentInitial.dto.ts
+++ src/modules/student/dto/create-studentInitial.dto.ts
@@ -1,9 +1,41 @@
+import {
+  IsArray,
+  IsEmail,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  Max,
+  Min,
+} from 'class-validator';
+
 export class CreateStudentInitialDto {
+  @IsEmail()
   email: string;
+
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   courseCompletion: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   courseEngagement: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   projectDegree: number;
+
+  @IsNumber()
+  @Min(0)
+  @Max(5)
   teamProjectDegree: number;
+
+  @IsArray()
+  @IsNotEmpty()
+  @IsString({ each: true })
   bonusProjectUrls: string[];
+
   [key: string]: any;
 }
