59151051e1569048cef0e952788e4d6600c21662
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-30 09:57:32 +0100

    remove id from dto

--- src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
+++ src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
@@ -1,7 +1,6 @@
 import { NewRecruiterEntity } from 'src/types';
 
 export class CreateHrRecruiterDto implements NewRecruiterEntity {
-  id: number;
   email: string;
   fullName: string;
   company: string;
