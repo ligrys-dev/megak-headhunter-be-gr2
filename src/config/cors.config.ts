053a6771c8c1a94e80716dc31c9a84eab6fba982
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-30 10:24:49 +0100

    fix cors config

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,8 +1,6 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
-import { ConfigService } from '@nestjs/config';
 
-export const getCorsConfig = (configService: ConfigService) =>
-  ({
-    origin: configService.get('CORS_ORIGIN'),
-    credentials: true,
-  }) as CorsOptions;
+export const getCorsConfig = {
+  origin: process.env.CORS_ORIGIN,
+  credentials: true,
+} as CorsOptions;
