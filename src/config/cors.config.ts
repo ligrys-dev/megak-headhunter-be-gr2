335b3a8f6a83a997fda6874e31d20caef3e926fa
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-11-30 09:56:42 +0100

    fix in configs

--- src/config/cors.config.ts
+++ src/config/cors.config.ts
@@ -1,6 +1,8 @@
 import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+import { ConfigService } from '@nestjs/config';
 
-export const getCorsConfig = {
-  origin: process.env.CORS_ORIGIN,
-  credentials: true,
-} as CorsOptions;
+export const getCorsConfig = (configService: ConfigService) =>
+  ({
+    origin: configService.get('CORS_ORIGIN'),
+    credentials: true,
+  }) as CorsOptions;
--- src/config/mailer.config.ts
+++ src/config/mailer.config.ts
@@ -1,34 +1,33 @@
-export const getMailerConfig = () => ({
-  transport: `smtp://admin:admin1@localhost:2500`, //XXX Mailsluper -> in future move to real smtp
-  defaults: {
-    from: 'noreply@megak.headhunter.com',
-  },
-  template: {
-    dir: '.',
-    options: {
-      strict: true,
+import { ConfigService } from '@nestjs/config';
+
+export const getMailerConfig = (configService: ConfigService) => {
+  const isSmtpMock = configService.get('USE_SMTP_MOCK');
+
+  const transportConfig = isSmtpMock
+    ? {
+        transport: `smtp://admin:admin1@localhost:2500`, // XXX Mailsluper
+      }
+    : {
+        transport: {
+          host: configService.get('EMAIL_HOST'),
+          port: +configService.get('EMAIL_PORT'),
+          auth: {
+            user: configService.get('EMAIL_USER'),
+            pass: configService.get('EMAIL_PASSWORD'),
+          },
+        },
+      };
+
+  return {
+    ...transportConfig,
+    defaults: {
+      from: 'noreply@megak.headhunter.com',
     },
-  },
-});
-
-// import { ConfigService } from '@nestjs/config';
-
-// export const getMailerConfig = (configService: ConfigService) => ({
-//   transport: {
-//     host: configService.get('EMAIL_HOST'),
-//     port: +configService.get('EMAIL_PORT'),
-//     auth: {
-//       user: configService.get('EMAIL_USER'),
-//       pass: configService.get('EMAIL_PASSWORD'),
-//     },
-//   },
-//   defaults: {
-//     from: 'noreply@megak.headhunter.com',
-//   },
-//   template: {
-//     dir: '.',
-//     options: {
-//       strict: true,
-//     },
-//   },
-// });
+    template: {
+      dir: '.',
+      options: {
+        strict: true,
+      },
+    },
+  };
+};
