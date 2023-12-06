c62212c0d967cecee6985bf315e0d3eab76fe3f1
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-06 11:19:24 +0100

    add relation to dtos

--- src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
+++ src/modules/hr-recruiter/dto/create-hr-recruiter.dto.ts
@@ -2,10 +2,12 @@ import {
   IsEmail,
   IsInt,
   IsNotEmpty,
+  IsOptional,
   IsString,
   Max,
   Min,
 } from 'class-validator';
+import { User } from 'src/modules/user/entities/user.entity';
 import { NewRecruiterInterface } from 'src/types';
 
 export class CreateHrRecruiterDto implements NewRecruiterInterface {
@@ -24,4 +26,9 @@ export class CreateHrRecruiterDto implements NewRecruiterInterface {
   @Min(1)
   @Max(999)
   maxReservedStudents: number;
+
+  @IsOptional()
+  user: User;
+
+  [key: string]: any;
 }
