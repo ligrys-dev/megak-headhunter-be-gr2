2773240c46f71cf8e93f034873cdc4e8f3b2605b
maclig92 <123300551+maclig92@users.noreply.github.com>
2023-12-06 10:03:45 +0100

    change properties in interface student filter to be optional

--- src/types/student/student-filters.interface.ts
+++ src/types/student/student-filters.interface.ts
@@ -1,12 +1,12 @@
 export interface StudentFilters {
-  courseCompletion: number;
-  courseEngagement: number;
-  projectDegree: number;
-  teamProjectDegree: number;
-  'profile.expectedContractType': number;
-  'profile.expectedTypeWork': number;
-  'profile.expectedSalary': number;
-  'profile.canTakeApprenticeship': number;
-  'profile.monthsOfCommercialExp': number;
+  courseCompletion?: number;
+  courseEngagement?: number;
+  projectDegree?: number;
+  teamProjectDegree?: number;
+  'profile.expectedContractType'?: number;
+  'profile.expectedTypeWork'?: number;
+  'profile.expectedSalary'?: number;
+  'profile.canTakeApprenticeship'?: number;
+  'profile.monthsOfCommercialExp'?: number;
   [key: string]: number;
 }
