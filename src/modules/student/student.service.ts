1cf56c542c28f74d712a842cbf10e36b7caa98d9
Krzysztof Iwanski <k84iwanski@gmail.com>
2023-12-14 21:22:44 +0100

    fix variable

--- src/modules/student/student.service.ts
+++ src/modules/student/student.service.ts
@@ -179,7 +179,7 @@ export class StudentService {
   }
 
   async markAvailable(email: string): Promise<StudentInitial> {
-    const student = await this.findOneInitialProfile(email);
+    const { student } = await this.findOneInitialProfile(email);
     student.status = StudentStatus.AVAILABLE;
     return await student.save();
   }
