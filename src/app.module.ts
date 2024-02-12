c6ef3ef24d46919d8849de77b4725668f68d992b
Krzysztof Iwanski <k84iwanski@gmail.com>
2024-02-12 21:42:27 +0100

    removed a few lines from app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './common/database/database.module';
@@ -30,9 +28,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     CacheModule.register({ isGlobal: true }),
     ConfigModule.forRoot({ isGlobal: true }),
   ],
-  controllers: [AppController],
   providers: [
-    AppService,
     { provide: APP_GUARD, useClass: RolesGuard },
     { provide: APP_GUARD, useClass: JwtAuthGuard },
   ],
